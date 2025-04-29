dc9f052edc3e5ce9ff8bc9096c54f93f69cc5a62
hmonroy-2020131 <hmonroy-2020131@kinal.edu.gt>
2025-04-29 09:22:58 -0600

    Usuarios Terminado.

---
+++ src/user/user.routes.js
@@ -0,0 +1,54 @@
+import { Router } from "express";
+import { check } from "express-validator";
+import { updateUser, deleteUser, getUsers, updateUserRole} from "./user.controller.js";
+import { validarCampos } from "../middlewares/validar-campos.js";
+import { validarJWT } from "../middlewares/validar-jwt.js";
+import { existeUsuarioById } from "../helpers/db-validator.js";
+import { tieneRole } from "../middlewares/validar-roles.js";
+
+const router = Router();
+
+router.get(
+    '/',
+    getUsers
+);
+
+router.put(
+    "/:id",
+    [
+        validarJWT,
+        tieneRole("ADMIN", "CLIENT"),
+        check("id", "Invalid ID").isMongoId(),
+        check("id").custom(existeUsuarioById),
+        check("email").optional().isEmail(),
+        validarCampos,
+    ],
+    updateUser
+);
+
+router.delete(
+    "/:id",
+    [
+        validarJWT,
+        tieneRole("ADMIN", "CLIENT"),
+        check("id", "Invalid ID").isMongoId(),
+        check("id").custom(existeUsuarioById),
+        validarCampos,
+    ],
+    deleteUser
+);
+
+router.put(
+    "/update-role/:id",
+    [
+        validarJWT,
+        tieneRole("ADMIN"),
+        check("id", "Invalid ID").isMongoId(),
+        check("role", "Invalid role").isIn(["ADMIN", "CLIENT"]),
+        validarCampos,
+    ],
+    updateUserRole
+);
+
+
+export default router;
