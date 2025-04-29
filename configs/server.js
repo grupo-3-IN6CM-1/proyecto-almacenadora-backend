5fd950a36e9da13d40be2b8f13a3c534b75a8bb6
hmonroy-2020131 <hmonroy-2020131@kinal.edu.gt>
2025-04-29 08:33:09 -0600

    Login terminado.

--- configs/server.js
+++ configs/server.js
@@ -6,6 +6,7 @@ import helmet from 'helmet';
 import morgan from 'morgan';
 import { dbConnection } from './mongo.js';
 import limiter from '../src/middlewares/validar-cant-peticion.js';
+import authRoutes from '../src/auth/auth.routes.js'
 
  
 const middlewares = (app) => {
@@ -18,7 +19,7 @@ const middlewares = (app) => {
 }
 
 const routes =(app) => {
-
+    app.use("/Almacenadora/v1/auth", authRoutes);
 }
  
 const conectarDB = async () => {
